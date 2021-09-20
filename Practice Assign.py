d41d583d83ae67878f43c7af417970c67011f514
ayoubSDSU <91095946+ayoubSDSU@users.noreply.github.com>
2021-09-20 14:14:32 -0700

    Add files via upload

---
+++ Practice Assign.py
@@ -0,0 +1,149 @@
+x = int(0)
+
+#____________________________________________________
+print("Match 1:")
+Number_of_Matches = (1+x)
+Home_Team1 = input("Enter Home Team Name:")
+print (Home_Team1)
+
+Home_Team_Score1 = int(input("Enter Home Team Score:"))
+print (Home_Team_Score1)
+
+Away_Team1 = input("Enter Away Team Name:")
+print (Away_Team1)
+
+Away_Team_Score1 = int(input("Enter Away Team Score:"))
+print (Away_Team_Score1)
+
+#____________________________________________________
+print ("____________________________________________________")
+
+
+print("Match 2:")
+
+Home_Team2 = input("Enter Home Team Name:")
+print (Home_Team2)
+
+Home_Team_Score2 = int(input("Enter Home Team Score:"))
+print (Home_Team_Score2)
+
+Away_Team2 = input("Enter Away Team Name:")
+print (Away_Team2)
+
+Away_Team_Score2 = int(input("Enter Away Team Score:"))
+print (Away_Team_Score2)
+
+#____________________________________________________
+print ("____________________________________________________")
+#____________________________________________________
+
+print("Match 3:")
+
+Home_Team3 = input("Enter Home Team Name:")
+print (Home_Team3)
+
+Home_Team_Score3 = int(input("Enter Home Team Score:"))
+print (Home_Team_Score3)
+
+Away_Team3 = input("Enter Away Team Name:")
+print (Away_Team3)
+
+Away_Team_Score3 = int(input("Enter Away Team Score:"))
+print (Away_Team_Score3)
+
+#____________________________________________________
+print ("____________________________________________________")
+#____________________________________________________
+
+
+print("Match 4:")
+
+Home_Team4 = input("Enter Home Team Name:")
+print (Home_Team4)
+
+Home_Team_Score4 = int(input("Enter Home Team Score:"))
+print (Home_Team_Score4)
+
+Away_Team4 = input("Enter Away Team Name:")
+print (Away_Team4)
+
+Away_Team_Score4 = int(input("Enter Away Team Score:"))
+print (Away_Team_Score4)
+
+#____________________________________________________
+print ("____________________________________________________")
+#____________________________________________________
+
+
+
+print("Match 5:")
+
+Home_Team5 = input("Enter Home Team Name:")
+print (Home_Team5)
+
+Home_Team_Score5 = int(input("Enter Home Team Score:"))
+print (Home_Team_Score5)
+
+Away_Team5 = input("Enter Away Team Name:")
+print (Away_Team5)
+
+Away_Team_Score5 = int(input("Enter Away Team Score:"))
+print (Away_Team_Score5)
+
+#____________________________________________________
+print ("____________________________________________________")
+#____________________________________________________
+
+
+print("Match 6:")
+
+Home_Team6 = input("Enter Home Team Name:")
+print (Home_Team1)
+
+Home_Team_Score6 = int(input("Enter Home Team Score:"))
+print (Home_Team_Score6)
+
+Away_Team6 = input("Enter Away Team Name:")
+print (Away_Team6)
+
+Away_Team_Score6 = int(input("Enter Away Team Score:"))
+print (Away_Team_Score6)
+
+#____________________________________________________
+print ("____________________________________________________")
+#____________________________________________________
+
+
+print("Match 7:")
+
+Home_Team7 = input("Enter Home Team Name:")
+print (Home_Team7)
+
+Home_Team_Score7 = int(input("Enter Home Team Score:"))
+print (Home_Team_Score7)
+
+Away_Team7 = input("Enter Away Team Name:")
+print (Away_Team7)
+
+Away_Team_Score7 = int(input("Enter Away Team Score:"))
+print (Away_Team_Score7)
+
+#____________________________________________________
+print ("____________________________________________________")
+#____________________________________________________
+
+
+print ("Type Done to see Statistics")
+text = input()
+
+if (text == 'Done'):
+        print ('Stats')
+        print("Thank You")
+elif (text != 'Done'):
+    print ("Type Done to see Statistics")
+#____________________________________________________
+print ("____________________________________________________")
+#____________________________________________________
+
+#Stat
+s
